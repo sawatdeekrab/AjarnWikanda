91af6f02cddd442e97477381f79dee030b163736
Thanee Ananitthi <sawatdeekrab@gmail.com>
2018-06-28 22:58:58 +0700

    update title update research

--- frontend/src/app/biography.component.ts
+++ frontend/src/app/biography.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-biography',
+  styleUrls: ['./biography.component.css'],
   templateUrl: './biography.component.html'
 })
 export class BiographyComponent  {
--- frontend/src/app/mock-researches.ts
+++ frontend/src/app/mock-researches.ts
@@ -1,6 +1,9 @@
 import { Research } from './research';
 
 export const RESEARCHES: Research[] = [
+    { year: 2018, title: 'Estimating Parameter for the Mixture Generalized Gamma Distribution.', language: 'English', publishing: 'Conference Proceedings', downloadLink: 'https://dl.acm.org/citation.cfm?id=3177492' },
+    { year: 2017, title: 'Parameter Estimation of the Crack Lifetime Distribution Using the Expectation–Maximization Algorithm.', language: 'English', publishing: 'Journals', downloadLink: 'http://dx.doi.org/10.17654/MS102050933' },
+    { year: 2017, title: 'A New Hybrid Heuristic for Minimizing Total Flow Time in Permutation Flow Shop.', language: 'English', publishing: 'Conference Proceedings', downloadLink: 'https://dl.acm.org/citation.cfm?id=3133814' },
     { year: 2017, title: 'The Generation of Random Variables Having Mixture Generalized Gamma Distribution by the Acceptance-Rejection Method.', language: 'Thai', publishing: 'Conference Proceedings', downloadLink: 'https://drive.google.com/open?id=0B-PcDocGk261cVYyNUh1dmJVMHc' },
     { year: 2017, title: 'Generating Random Variables for a New Two-parameter Crack Distribution by the Acceptance-Rejection Method.', language: 'Thai', publishing: 'Conference Proceedings', downloadLink: 'https://drive.google.com/open?id=0B-PcDocGk261Q3hnYnAyanJEQkU' },
     { year: 2017, title: 'Point Estimation of Birnbuam-Saunders Distribution Using EM-Algorithm.', language: 'English', publishing: 'Journals', downloadLink: 'https://drive.google.com/open?id=0B-PcDocGk261dFM3N3FOUUE1cGs' },
